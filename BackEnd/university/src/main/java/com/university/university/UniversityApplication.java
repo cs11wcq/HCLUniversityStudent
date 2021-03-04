d59194bd9811f7de0d69556feb7c2b7c721aef14
wongb2 <57199009+wongb2@users.noreply.github.com>
2021-03-04 00:29:55 -0500

    adding university files

---
+++ BackEnd/university/src/main/java/com/university/university/UniversityApplication.java
@@ -0,0 +1,16 @@
+package com.university.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * All methods are working now
+ */
+@SpringBootApplication
+public class UniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplication.class, args);
+	}
+
+}
